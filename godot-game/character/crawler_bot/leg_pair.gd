77193fad4a93e6c4dbe9621ea39f5d2d0b7c8dde
almic <mick.ashton@flare-esports.net>
2026-03-14 12:45:07 -0600

    Add CrawlerCharacter, initial work on crawlers

---
+++ godot-game/character/crawler_bot/leg_pair.gd
@@ -0,0 +1,10 @@
+class_name CrawlerLegPair extends Node3D
+
+@export var root_left: Node3D
+@export var root_right: Node3D
+
+@export var cast_left: ShapeCast3D
+@export var cast_right: ShapeCast3D
+
+@export var target_left: Marker3D
+@export var target_right: Marker3D
