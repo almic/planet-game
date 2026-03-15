54471d1dd58e4fb56d20a3b67a8a1ba701c67ebb
almic <mick.ashton@flare-esports.net>
2026-03-15 16:49:46 -0600

    Work on Crawler, rework legs
    - Also updated test world stairs and elevator
    - Crawlers will now update individual legs instead of using
      specific pairs. Legs are still expected to come in pairs.

--- godot-game/character/crawler_bot/CrawlerCharacter.gd
+++ godot-game/character/crawler_bot/CrawlerCharacter.gd
@@ -1,32 +1,31 @@
 @tool
 class_name CrawlerCharacter extends CharacterController
 
-@export var leg_pairs: Array[CrawlerLegPair]
+@export var legs: Array[CrawlerLeg]
 
 
 
 func _ready() -> void:
 
-    for pair in leg_pairs:
-        # Ensure targets are top-level
-        pair.target_left.top_level = true
-        pair.target_right.top_level = true
-
+    for leg in legs:
         # Copy collision mask to casters
-        pair.cast_left.collision_mask = collision_mask
-        pair.cast_right.collision_mask = collision_mask
+        leg.shape_cast.collision_mask = collision_mask
+
 
 func _handle_input() -> void:
     desired_direction = Vector3.BACK
     desired_speed = 1.0
 
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+    _update_legs(state)
+
+    super._integrate_forces(state)
+
+func _update_legs(state: PhysicsDirectBodyState3D) -> void:
+    for leg in legs:
+        leg.update()
+
+
 func _calculate_ground_force(state: PhysicsDirectBodyState3D) -> void:
 
-    for pair in leg_pairs:
-        # Move targets to collision point
-        if pair.cast_left.is_colliding():
-            pair.target_left.transform.origin = pair.cast_left.get_collision_point(0)
-        if pair.cast_right.is_colliding():
-            pair.target_right.transform.origin = pair.cast_right.get_collision_point(0)
-
-    spring_force = -state.total_gravity * mass
+    desired_acceleration = -state.total_gravity
--- godot-game/character/crawler_bot/leg_pair.gd
+++ godot-game/character/crawler_bot/leg_pair.gd
@@ -1,10 +1,30 @@
-class_name CrawlerLegPair extends Node3D
+@tool
+class_name CrawlerLeg extends Node3D
 
-@export var root_left: Node3D
-@export var root_right: Node3D
+@export var shape_cast: ShapeCast3D
+@export var target: Marker3D
 
-@export var cast_left: ShapeCast3D
-@export var cast_right: ShapeCast3D
+var is_grounded: bool = false
 
-@export var target_left: Marker3D
-@export var target_right: Marker3D
+func _ready() -> void:
+    if not shape_cast:
+        for child in find_children('', 'ShapeCast3D', false):
+            shape_cast = child as ShapeCast3D
+            if shape_cast:
+                break
+    if not target:
+        for child in find_children('', 'Marker3D', false):
+            target = child as Marker3D
+            if target:
+                break
+
+    # Ensure target is top-level
+    target.top_level = true
+
+func update() -> void:
+    # Move targets to collision point
+    if shape_cast.is_colliding():
+        target.transform.origin = shape_cast.get_collision_point(0)
+        is_grounded = true
+    else:
+        is_grounded = false
